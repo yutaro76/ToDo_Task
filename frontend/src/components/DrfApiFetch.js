f94b14efdd2627b9109c28e3c1c3669c15b2f67b
yutaro okuda <yutaro.okuda.76@gmail.com>
2024-03-10 10:23:10 +0800

    api get id

--- frontend/src/components/DrfApiFetch.js
+++ frontend/src/components/DrfApiFetch.js
@@ -4,12 +4,20 @@ import axios from 'axios';
 const DrfApiFetch = () => {
 
   const [tasks, setTasks] = useState([]);
+  const [selectedTask, setSelected] = useState([])
+  const [id, setId] = useState(1)
 
   useEffect(() => {
     fetch('http://127.0.0.1:8000/api/tasks/', {method: 'GET'})
     .then(res => res.json())
     .then(data => {setTasks(data)})
   },[]);
+
+  const getTask = () => {
+    fetch(`http://127.0.0.1:8000/api/tasks/${id}/`, {method: 'GET'})
+    .then(res => res.json())
+    .then(data => {setSelected(data)})
+  }
 
   return (
     <div>
@@ -18,6 +26,13 @@ const DrfApiFetch = () => {
           tasks.map(task => <li key={task.id}>{task.title} {task.id}</li>)
         }
       </ul>
+
+      Set id <br/>
+      <input type='text' value={id} onChange={evt=>{setId(evt.target.value)}}/>
+      <br/>
+      <button type='button' onClick={()=>getTask()}>Get Task</button>
+      <h3>{selectedTask.title} {selectedTask.id}</h3>
+      
     </div>
   )
 }
