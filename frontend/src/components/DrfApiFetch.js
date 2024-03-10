00cbec05218c0d1c8f2878eb1a6b2fccf6c1d4b4
yutaro okuda <yutaro.okuda.76@gmail.com>
2024-03-10 11:49:17 +0800

    api create

--- frontend/src/components/DrfApiFetch.js
+++ frontend/src/components/DrfApiFetch.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 
 const DrfApiFetch = () => {
 
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([])
   const [selectedTask, setSelected] = useState([])
+  const [editedTask, setEditedTask] = useState({id:'', title:''})
   const [id, setId] = useState(1)
 
   useEffect(() => {
@@ -21,6 +22,27 @@ const DrfApiFetch = () => {
   const deleteTask = (id) => {
     fetch(`http://127.0.0.1:8000/api/tasks/${id}/`, {method: 'DELETE'})
     .then(() => {setTasks(tasks.filter(task => task.id !== id)); setSelected([]); setId('')})
+  }
+
+  const newTask = (task) => {
+    const data = {
+      title: task.title
+    }
+    fetch('http://127.0.0.1:8000/api/tasks/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(data => setTasks([...tasks, data]))
+  }
+
+  const handleInputChange = () => evt => {
+    const value = evt.target.value
+    const name = evt.target.name
+    setEditedTask({...editedTask, [name]:value})
   }
 
   return (
@@ -41,6 +63,14 @@ const DrfApiFetch = () => {
       <button type='button' onClick={()=>getTask()}>Get Task</button>
       
       <h3>{selectedTask.title} {selectedTask.id}</h3>
+
+      <input type='text' name='title'
+      value = {editedTask.title}
+      onChange={handleInputChange()}
+      placeholder='New Task?'
+      required/>
+
+      <button onClick={()=>newTask(editedTask)}>Create</button>
       
     </div>
   )
